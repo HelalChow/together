8dcfd9611448e0bbc2860793bfb049bf09d80e21
hchowdhury3273 <hc2324@nyu.edu>
2019-09-21 16:10:54 -0400

    detailview

--- together/together/ProfileViewController.swift
+++ together/together/ProfileViewController.swift
@@ -9,22 +9,49 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
+    
+    @IBOutlet weak var eventTableView: UITableView!
+    
+    var events: [Event] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        eventTableView.delegate = self
+        eventTableView.dataSource = self
+        events = createArray()
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func createArray() -> [Event] {
+        let tempBool: [Event] = [Event.e1, Event.e2, Event.e3, Event.e4]
+        print("1")
+        return tempBool
+        
     }
-    */
+    
+    
+}
 
+extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return events.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let event = events[indexPath.row]
+        print("10")
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EventsCell") as! EventsViewCell
+        print("100")
+        
+        cell.setEvents(event: event)
+        
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+   
+   
 }
