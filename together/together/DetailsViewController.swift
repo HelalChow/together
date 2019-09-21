b4dfad691970f019392628393d27ab95c555d17e
hchowdhury3273 <hc2324@nyu.edu>
2019-09-21 14:13:23 -0400

    added files

---
+++ together/together/DetailsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  DetailsViewController.swift
+//  together
+//
+//  Created by Helal Chowdhury on 9/21/19.
+//  Copyright © 2019 Helal. All rights reserved.
+//
+
+import Foundation
---
+++ together/together/Event.swift
@@ -0,0 +1,9 @@
+//
+//  Event.swift
+//  together
+//
+//  Created by Helal Chowdhury on 9/21/19.
+//  Copyright © 2019 Helal. All rights reserved.
+//
+
+import Foundation
