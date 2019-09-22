cec5ac74e9b26f597918de4f47fd6aa8f66986d5
hchowdhury3273 <hc2324@nyu.edu>
2019-09-21 21:08:14 -0400

    contqact

---
+++ together/together/Contact.swift
@@ -0,0 +1,21 @@
+//
+//  Contact.swift
+//  together
+//
+//  Created by Helal Chowdhury on 9/21/19.
+//  Copyright © 2019 Helal. All rights reserved.
+//
+
+import Foundation
+import Contacts
+
+class Contact {
+    
+    let firstName: String
+    let familyName: String
+    let number: String
+
+    init(firstName: String, familyName: String, number: String) {
+        self.firstName = firstName
+        self.familyName = familyName
+}
