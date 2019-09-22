85118748a820dafb6e35a072b0acbdabaa7fc40b
hchowdhury3273 <hc2324@nyu.edu>
2019-09-22 01:28:24 -0400

    remove

---
+++ together/together/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.swift
+//  together
+//
+//  Created by Helal Chowdhury on 9/22/19.
+//  Copyright © 2019 Helal. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+  
+}
