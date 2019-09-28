e19a32b89cc6985d2b2a20bcfd0ac15dee174a3e
hchowdhury3273 <hc2324@nyu.edu>
2019-09-28 03:41:15 -0400

    final round 1

--- together/together/ProfileViewController.swift
+++ together/together/ProfileViewController.swift
@@ -10,25 +10,25 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
-    @IBOutlet weak var eventTableView: UITableView!   
-    
+    @IBOutlet weak var eventTableView: UITableView!
+
     var events: [Event] = []
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         eventTableView.delegate = self
         eventTableView.dataSource = self
         events = createArray()
-        
+
     }
-    
+
     func createArray() -> [Event] {
         let tempBool: [Event] = [Event.e1, Event.e2, Event.e3, Event.e4]
         print("1")
         return tempBool
-        
+
     }
-    
+
     
 }
 
@@ -36,22 +36,22 @@ extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return events.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let event = events[indexPath.row]
         print("10")
-        
+
         let cell = tableView.dequeueReusableCell(withIdentifier: "EventsCell") as! EventsViewCell
         print("100")
-        
+
         cell.setEvents(event: event)
-        
+
         return cell
     }
-    
+
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-   
-   
+
+
 }
